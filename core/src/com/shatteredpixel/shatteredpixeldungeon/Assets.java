1de3d48a6c04b812f7e56e48b4e98b0797d0b0a5
crazywolf132 <crazywolf132@gmail.com>
2016-05-13 06:57:59 +1000

    Started adding 2 new monsters

--- core/src/com/shatteredpixel/shatteredpixeldungeon/Assets.java
+++ core/src/com/shatteredpixel/shatteredpixeldungeon/Assets.java
@@ -6,12 +6,12 @@ public class Assets {
 	public static final String ARCS_BG		= "arcs1.png";
 	public static final String ARCS_FG		= "arcs2.png";
 	public static final String DASHBOARD	= "dashboard.png";
-	
+
 	public static final String BANNERS	= "banners.png";
 	public static final String BADGES	= "badges.png";
 	public static final String LOCKED	= "locked_badge.png";
 	public static final String AMULET	= "amulet.png";
-	
+
 	public static final String CHROME	= "chrome.png";
 	public static final String ICONS	= "icons.png";
 	public static final String STATUS	= "status_pane.png";
@@ -28,13 +28,13 @@ public class Assets {
 	public static final String HUNTRESS	= "ranger.png";
 	public static final String AVATARS	= "avatars.png";
 	public static final String PET		= "pet.png";
-	
+
 	public static final String SURFACE	= "surface.png";
-	
+
 	public static final String FIREBALL		= "fireball.png";
 	public static final String SPECKS		= "specks.png";
 	public static final String EFFECTS		= "effects.png";
-	
+
 	public static final String RAT		= "rat.png";
 	public static final String GNOLL	= "gnoll.png";
 	public static final String CRAB		= "crab.png";
@@ -80,13 +80,13 @@ public class Assets {
 	public static final String ITEMS	= "items.png";
 	public static final String PLANTS	= "plants.png";
 	public static final String TRAPS    = "traps.png";
-	
+
 	public static final String TILES_SEWERS	= "tiles0.png";
 	public static final String TILES_PRISON	= "tiles1.png";
 	public static final String TILES_CAVES	= "tiles2.png";
 	public static final String TILES_CITY	= "tiles3.png";
 	public static final String TILES_HALLS	= "tiles4.png";
-	
+
 	public static final String WATER_SEWERS	= "water0.png";
 	public static final String WATER_PRISON	= "water1.png";
 	public static final String WATER_CAVES	= "water2.png";
@@ -96,24 +96,24 @@ public class Assets {
 	public static final String WEAK_FLOOR   = "custom_tiles/weak_floor.png";
 	public static final String PRISON_QUEST = "custom_tiles/prison_quests.png";
 	public static final String PRISON_EXIT  = "custom_tiles/prison_exit.png";
-	
+
 	public static final String BUFFS_SMALL	= "buffs.png";
 	public static final String BUFFS_LARGE	= "large_buffs.png";
 	public static final String SPELL_ICONS	= "spell_icons.png";
 	public static final String CONS_ICONS   = "consumable_icons.png";
-	
+
 	public static final String PIXELFONT    = "pixel_font.png";
 	public static final String FONT1X       = "font1x.png";
 	public static final String FONT2X       = "font2x.png";
-	
+
 	public static final String THEME	= "theme.mp3";
 	public static final String TUNE		= "game.mp3";
 	public static final String HAPPY	= "surface.mp3";
-	
+
 	public static final String SND_CLICK	= "snd_click.mp3";
 	public static final String SND_BADGE	= "snd_badge.mp3";
 	public static final String SND_GOLD		= "snd_gold.mp3";
-	
+
 	public static final String SND_OPEN		= "snd_door_open.mp3";
 	public static final String SND_UNLOCK	= "snd_unlock.mp3";
 	public static final String SND_ITEM		= "snd_item.mp3";
@@ -157,4 +157,5 @@ public class Assets {
 	public static final String SND_BEE      = "snd_bee.mp3";
 	public static final String SND_DEGRADE  = "snd_degrade.mp3";
 	public static final String SND_MIMIC    = "snd_mimic.mp3";
+	public static final String SLIME    = "slime.png"
 }
